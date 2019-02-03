b8605a8f637c16b3cd5968c3a7d5ebc7e11d490b
deploy.sh <>
2019-02-03 11:37:07 +0100

    publish: Working on basic js structure and animating the arrow function
    generated from commit 59d02e8f619562d3453f65256758f1923e906d01

--- js/bundle.js
+++ js/bundle.js
@@ -1,28 +1,53 @@
 (function () {
   'use strict';
 
-  window.addEventListener('load', function () {
-    var io = new IntersectionObserver(
-      function (entries, observer) {
-        entries.forEach(function (element, index) {
-          if (element.isIntersecting) {
-            var img = element.target;
-            img.src = img.dataset.src;
-            observer.unobserve(img);
-          }
-        });
-      }, {
-        rootMargin: "300px",
-        threshold: [0]
-      }
-    );
-
-    document.querySelectorAll('img.lazyload')
-      .forEach(function (element, index) {
-        io.observe(element);
+  function arrowAnimation() {
+    const handleArrowAnimation = (entries, observer) => {
+      entries.forEach((element, index) => { 
+        const arrow_animate = 'splash__container--arrow-down-animate';
+        const arrow = element.target;
+        (element.intersectionRatio > 0.25) ? arrow.classList.add(arrow_animate) : arrow.classList.remove(arrow_animate);
       });
+    };
+
+    const io = new IntersectionObserver(handleArrowAnimation, {
+        rootMargin: '0px',
+        threshold: [0.25]
+      });
+
+    document.querySelectorAll('.splash__container--arrow-down')
+    .forEach(element => {
+      io.observe(element);
+    });
+  }
+
+  const domReady = new Promise((resolve, reject) => {
+    function completed() {
+      document.removeEventListener( 'load', completed );
+      window.removeEventListener( 'load', completed );
+      resolve();
+    }
+    
+    if ( document.readyState === 'complete' ||
+    ( document.readyState !== 'loading' && !document.documentElement.doScroll ) ) {
+      // Handle it asynchronously to allow scripts the opportunity to delay ready
+      window.setTimeout( completed );
+    
+    } else {
+      // Use the handy event callback
+      document.addEventListener( 'DOMContentLoaded', completed );
+      // A fallback to window.onload, that will always work
+      window.addEventListener('load', completed);
+    }
   });
 
-  console.log("Hello World");
+
+  async function ready (fn) {
+    await domReady;
+    return fn ? fn() : null;
+  }
+
+  ready()
+    .then(() => arrowAnimation());
 
 }());
