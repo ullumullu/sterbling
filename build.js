930dcd2850198438bd85769f037a5215be101f92
Sven Sterbling <sven@sterbling.com>
2022-11-10 19:17:05 +0100

    :boom: Support node 18 LTS for build

--- build.js
+++ build.js
@@ -1,151 +1,174 @@
-const metalsmith = require('metalsmith'),
-  branch = require('metalsmith-branch'),
-  collections = require('metalsmith-collections'),
-  excerpts = require('metalsmith-excerpts'),
-  markdown = require('metalsmith-markdown'),
-  permalinks = require('metalsmith-permalinks'),
-  serve = require('metalsmith-serve'),
-  layouts = require('metalsmith-layouts'),
-  inplace = require('metalsmith-in-place'),
-  publish = require('metalsmith-publish'),
-  sass = require('metalsmith-sass'),
-  dateFilter = require('nunjucks-date-filter'),
-  nunjucks = require('nunjucks'),
-  sitemap = require('metalsmith-mapsite'),
-  rollup = require('./metalsmith-rollup'),
-  uglify = require('metalsmith-uglify'),
-  devBuild = ((process.env.NODE_ENV || '').trim().toLowerCase() !== 'production'),
-  htmlmin = devBuild ? null : require('metalsmith-html-minifier'),
-  watch = devBuild ? require('metalsmith-watch') : null,
-  pkg = require('./package.json');
+const metalsmith = require("metalsmith"),
+  branch = require("metalsmith-branch"),
+  collections = require("metalsmith-collections"),
+  excerpts = require("metalsmith-excerpts"),
+  markdown = require("metalsmith-markdown"),
+  permalinks = require("metalsmith-permalinks"),
+  serve = require("metalsmith-serve"),
+  layouts = require("metalsmith-layouts"),
+  inplace = require("metalsmith-in-place"),
+  publish = require("metalsmith-publish"),
+  sass = require("metalsmith-sass"),
+  dateFilter = require("nunjucks-date-filter"),
+  nunjucks = require("nunjucks"),
+  sitemap = require("metalsmith-mapsite"),
+  rollup = require("./metalsmith-rollup"),
+  uglify = require("metalsmith-uglify"),
+  devBuild = (process.env.NODE_ENV || "").trim().toLowerCase() !== "production",
+  htmlmin = devBuild ? null : require("metalsmith-html-minifier"),
+  watch = devBuild ? require("metalsmith-watch") : null,
+  pkg = require("./package.json");
 
-console.log('Starting site build');
-console.log((devBuild ? 'Development' : 'Production'), 'build, version', pkg.version);
+console.log("Starting site build");
+console.log(
+  devBuild ? "Development" : "Production",
+  "build, version",
+  pkg.version
+);
 
 const siteMeta = {
   site: {
     devBuild: devBuild,
     version: pkg.version,
-    title: 'sterbling.com',
-    url: 'https://sterbling.com',
-    domain: devBuild ? 'http://127.0.0.1' : 'https://sterbling.com'
-  }
-}
+    title: "sterbling.com",
+    url: "https://sterbling.com",
+    domain: devBuild ? "http://127.0.0.1" : "https://sterbling.com",
+  },
+};
 
 const templateConfig = {
-  engine: 'nunjucks',
-  directory: 'src/layouts',
-  watch: 'true',
-  noCache: 'true',
+  engine: "nunjucks",
+  directory: "src/layouts",
+  watch: "true",
+  noCache: "true",
   rename: true,
   cache: false,
-  exposeConsolidate: requires => {
-    const nunEnv = nunjucks
-      .configure('./src/layouts', {
-        watch: 'true',
-        noCache: 'true'
-      });
-    nunEnv.addFilter('date', dateFilter);
+  exposeConsolidate: (requires) => {
+    const nunEnv = nunjucks.configure("./src/layouts", {
+      watch: "true",
+      noCache: "true",
+    });
+    nunEnv.addFilter("date", dateFilter);
     requires.nunjucks = nunEnv;
-  }
-}
+  },
+};
 
 let siteBuild = metalsmith(__dirname)
   .clean(true)
   .metadata(siteMeta)
-  .source('./src')
-  .destination('./build')
-  .use(sass({
-    outputStyle: devBuild ? 'devBuild' : 'compressed',
-    includePaths: ['node_modules', 'src', '.']
-  }))
+  .source("./src")
+  .destination("./build")
+  .use(
+    sass({
+      outputStyle: devBuild ? "devBuild" : "compressed",
+      includePaths: ["node_modules", "src", "."],
+    })
+  )
   .use(publish())
   .use(markdown())
   .use(excerpts())
-  .use(collections({
-    posts: {
-      pattern: 'posts/**.html',
-      sortBy: 'publishDate',
-      reverse: true
-    }
-  }))
-  .use(branch(['!layouts/**.njk', 'pages/**', 'index.njk'])
-    .use(
+  .use(
+    collections({
+      posts: {
+        pattern: "posts/**.html",
+        sortBy: "publishDate",
+        reverse: true,
+      },
+    })
+  )
+  .use(
+    branch(["!layouts/**.njk", "pages/**", "index.njk"]).use(
       inplace({
         engineOptions: {
           filters: {
-            'date': dateFilter
+            date: dateFilter,
           },
-          noCache: 'true',
-          watch: 'true'
-        }
-      }))
+          noCache: "true",
+          watch: "true",
+        },
+      })
+    )
   )
   .use(layouts(templateConfig))
-  .use(branch(['pages/**.html'])
-    .use(permalinks({
-      pattern: ':title',
-      relative: false
-    }))
+  .use(
+    branch(["pages/**.html"]).use(
+      permalinks({
+        pattern: ":title",
+        relative: false,
+      })
+    )
   )
-  .use(branch('posts/**.html')
-    .use(permalinks({
-      pattern: 'posts/:title',
-      relative: false
-    }))
+  .use(
+    branch("posts/**.html").use(
+      permalinks({
+        pattern: "posts/:title",
+        relative: false,
+      })
+    )
   )
-  .use(rollup({
-    input: 'src/js/main.js', // Entry point
-    output: {
-      format: 'iife',
-      file: 'js/bundle.js',
-      sourcemap: devBuild, // This will be placed under "build/"
-    },
-  },
-  {
-    ignoreSources: true
-  }))
-  
-if (!devBuild) {
-  siteBuild.use(uglify({
-    concat:{file:"js/bundle.js"},
-    es: 6,
-    uglify: { sourceMap: devBuild ? { includeSources: true } : false },
-    removeOriginal: false,
-  }));
-}
+  .use(
+    rollup(
+      {
+        inputOptions: {
+          input: "src/js/main.js", // Entry point
+        },
+        outputOptions: {
+          format: "iife",
+          file: "js/bundle.js",
+          sourcemap: devBuild,
+        },
+      },
+      {
+        ignoreSources: true,
+      }
+    )
+  )
+  .use(
+    uglify({
+      concat: { file: "js/bundle.js" },
+      es: 6,
+      uglify: { sourceMap: devBuild ? { includeSources: true } : false },
+      removeOriginal: false,
+    })
+  );
 
-if (htmlmin) siteBuild.use(htmlmin())
-
+if (htmlmin) siteBuild.use(htmlmin());
 
 if (watch) {
-  siteBuild.use(serve({
-      port: 8080,
-      verbose: true
-    }))
-    .use(watch({
-      paths: {
-        "${source}/**/*": true, // every changed files will trigger a rebuild of themselves
-        "${source}/layouts/**/*": "**/*.njk", // every templates changed will trigger a rebuild of all files,
-        "${source}/styles/**/*": "**/*.scss", // every templates changed will trigger a rebuild of all files,
-        "${source}/js/**/*": "**/main.js" // every js changed will trigger a rebuild of main.js
-      },
-      livereload: true
-    }));
+  siteBuild
+    .use(
+      serve({
+        port: 8080,
+        verbose: true,
+      })
+    )
+    .use(
+      watch({
+        paths: {
+          "${source}/**/*": true, // every changed files will trigger a rebuild of themselves
+          "${source}/layouts/**/*": "**/*.njk", // every templates changed will trigger a rebuild of all files,
+          "${source}/styles/**/*": "**/*.scss", // every templates changed will trigger a rebuild of all files,
+          "${source}/js/**/*": "**/main.js", // every js changed will trigger a rebuild of main.js
+        },
+        livereload: true,
+      })
+    );
 }
 
 siteBuild
-  .use(sitemap({
-    hostname: siteMeta.site.domain + (siteMeta.site.rootpath || ''),
-    omitIndex: true
-  }))
+  .use(
+    sitemap({
+      hostname: siteMeta.site.domain + (siteMeta.site.rootpath || ""),
+      omitIndex: true,
+    })
+  )
   .build(function (err) {
     if (err) {
       console.log(err);
     } else {
-      console.log('Site build complete!');
+      console.log("Site build complete!");
       if (!watch) {
-        process.exit(0)
+        process.exit(0);
       }
     }
   });
--- metalsmith-rollup/index.js
+++ metalsmith-rollup/index.js
@@ -1,84 +1,89 @@
-const Debug = require('debug');
-const { sep, resolve, basename, dirname, join } = require('path');
-const rollup = require('rollup');
+const Debug = require("debug");
+const { sep, resolve, basename, dirname, join } = require("path");
+const rollup = require("rollup");
 
-const debug = Debug('metalsmith-rollup')
+const debug = Debug("metalsmith-rollup");
 
-function normalizePath (p) {
-  return p.split(sep).filter((q) => {
-    return typeof q === 'string' && q.length > 0
-  }).join('/')
+function normalizePath(p) {
+  return p
+    .split(sep)
+    .filter((q) => {
+      return typeof q === "string" && q.length > 0;
+    })
+    .join("/");
 }
 
-function getMetalsmithKey (files, p) {
-  p = normalizePath(p)
+function getMetalsmithKey(files, p) {
+  p = normalizePath(p);
 
   for (const key in files) {
     if (files.hasOwnProperty(key) && normalizePath(key) === p) {
-      return key
+      return key;
     }
   }
-  return null
+  return null;
 }
 
-function passthru (name, contents) {
+function passthru(name, contents) {
   return {
-    load (id) {
-      if (id === name) return contents
-    }
-  }
+    name: "passthru",
+    load: {
+      order: null,
+      handler: (id) => {
+        if (id === name) {
+          return contents;
+        }
+        return null;
+      },
+    },
+  };
 }
 
-module.exports = function plugin (config, pluginConfig) {
-  const outFile = config.output.file
+module.exports = function plugin(config, pluginConfig = {}) {
+  const { inputOptions, outputOptions } = config;
 
-  return function (files, metalsmith, done) {
-    if (config.input && typeof config.input === 'string') {
-      const inputPath = resolve(config.input)
-      if (!config.plugins) config.plugins = []
+  return async function (files, metalsmith) {
+    if (inputOptions.input && typeof inputOptions.input === "string") {
+      const inputPath = resolve(inputOptions.input);
+      if (!inputOptions.plugins) inputOptions.plugins = [];
       for (const file of Object.keys(files)) {
         if (resolve(metalsmith.source(), file) === inputPath) {
-          const contents = files[file].contents.toString()
-          config.plugins.unshift(passthru(inputPath, contents))
+          const contents = files[file].contents.toString();
+          inputOptions.plugins.unshift(passthru(inputPath, contents));
         }
       }
     }
-    return rollup.rollup(config)
-      .then((bundle) => {
-        return bundle.generate(config)
-      })
-      .then((result) => {
-        const output = result
-        const mapFile = output.map ? output.map.file : ''
-        const key = getMetalsmithKey(files, outFile) || outFile
+    const bundle = await rollup.rollup(inputOptions);
+    const result = await bundle.generate(outputOptions);
 
-        files[key] = {contents: output.code}
+    const output = result.output[0];
+    const mapFile = output.map ? output.map.file : "";
+    const key =
+      getMetalsmithKey(files, outputOptions.file) || outputOptions.file;
 
-        if (config.output.sourcemap && mapFile) {
-          const pathDirName = dirname(config.output.file);
-          const mapFileName = `${mapFile}.map`
-          const sourceMapKey = getMetalsmithKey(files, join(pathDirName, mapFileName)) || join(pathDirName, mapFileName)
-          files[sourceMapKey] = {contents: JSON.stringify(output.map, 3)}
+    files[key] = { contents: output.code };
 
-          files[key].contents += `\n//# sourceMappingURL=${mapFileName}`;
-        }
-
-        if (pluginConfig && pluginConfig.ignoreSources && output.modules) {
-          output.modules
-          .map(function getKey (file) {
-            const fullPathSrc = resolve(metalsmith._source);
-            const buildFile = file.split(fullPathSrc)[1];
-            return getMetalsmithKey(files, buildFile) || basename(file)
-          })
-          .forEach(function eachFile (file) {
-              delete files[file]
-            })
-        }
-        debug(`Successfully bundled file ${outFile}!`)
-        return done()
-      })
-      .catch((err) => {
-        done(err)
-      })
-  }
-}
+    if (outputOptions.sourceMap && mapFile) {
+      const pathDirName = dirname(outputOptions.file);
+      const mapFileName = `${mapFile}.map`;
+      const sourceMapKey =
+        getMetalsmithKey(files, join(pathDirName, mapFileName)) ||
+        join(pathDirName, mapFileName);
+      files[sourceMapKey] = { contents: JSON.stringify(output.map, 3) };
+    }
+    // Cleanup source files from build output
+    const { ignoreSources } = pluginConfig;
+    if (ignoreSources && output.modules) {
+      Object.keys(output.modules)
+        .map(function getKey(file) {
+          const fullPathSrc = resolve(metalsmith._source);
+          const buildFile = file.split(fullPathSrc)[1];
+          return getMetalsmithKey(files, buildFile) || basename(file);
+        })
+        .forEach(function eachFile(file) {
+          delete files[file];
+        });
+    }
+    debug(`Successfully bundled file ${outputOptions.file}!`);
+  };
+};
